ffc59f26f57907133f18911799de1fb92750fdd2
Joan Marie Verba <verba001@umn.edu>
2016-08-24 14:30:45 -0500

    print button

--- src/DirectoryList.jsx
+++ src/DirectoryList.jsx
@@ -3,6 +3,7 @@ import DirectoryItem from './DirectoryItem.jsx';
 import elementalStyles from '../node_modules/elemental/less/elemental.less';
 import { Button, Alert, Spinner, Row, Col, Form, FormField, FormInput } from 'elemental';
 import { browserHistory } from 'react-router';
+import { StyleSheet, css } from 'aphrodite';
 
 var directoryStyle = {
     marginTop: "10px",
@@ -11,14 +12,36 @@ var directoryStyle = {
     msTransition: 'all'
  };
 
+ var printButton = {
+   height: '28px',
+   textAlign: 'center',
+   color: "black",
+   border: "2px solid black",
+   fontWeight: "bold",
+   display: "block",
+   margin: "0 auto",
+}
+
+const printbtn = StyleSheet.create({
+  btn: {
+        "@media print" : {
+             display: "none",
+        }
+    }
+});
 
 export default class DirectoryList extends Component {
 
+  handlePrintButtonClick(e) {   //method
+    e.preventDefault(); //prevents form submission from deleting current page context
+    window.print();
+  }
 
   render() {
     console.log("list ", this.props.entryArray);
     return (
       <div style={directoryStyle}>
+      <div><Button size="sm" style={printButton} className={css(printbtn.btn)} onClick={this.handlePrintButtonClick.bind(this)}>Print this page</Button></div>
       {this.props.entryArray.map((item) => {
         return (
           <DirectoryItem
