f382f52b59c1224883899857eff161f5e3406afe
Joan Marie Verba <verba001@umn.edu>
2016-08-26 09:31:37 -0500

    updated README

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -5,7 +5,7 @@ import { browserHistory } from 'react-router';
 
 var logInPageStyle = {
   // backgroundImage: 'url("http://jmvtestsite.com/wp-content/uploads/2016/08/background-1430105_1280.png")',
-  backgroundImage: 'url("http://jmvtestsite.com/wp-content/uploads/2016/08/tablet-new.jpg")',
+  backgroundImage: 'url("http://jmvtestsite.com/wp-content/uploads/2016/08/tablet-new-1.jpg")',
   // backgroundColor: "green",
   width: '100%',
   minHeight: '100%',
